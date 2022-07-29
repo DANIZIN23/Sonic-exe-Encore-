9f4ec0336d8307cfc6a1fcfa1a01715c5f78f451
Danibomfin <98532797+Danibomfin@users.noreply.github.com>
2022-07-29 09:26:37 -0300

    Add files via upload

---
+++ assets/preload/data/you-cant-run-encore/pauseOFF.lua
@@ -0,0 +1,5 @@
+function onPause()
+    debugPrint("you can't pause when you run"); 
+    playSound("missnote1");
+	return Function_Stop;
+end
---
+++ assets/preload/data/you-cant-run-encore/skakeeeeee.lua
@@ -0,0 +1,3 @@
+function opponentNoteHit(id,data,type,sus)
+    triggerEvent('Screen Shake','1, 0.01, 0.00','0.00, 0.00')
+ end
